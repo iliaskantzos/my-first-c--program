6f539453648e14555a4049a0e573fad6386abe40
iliaskantzos <eliaskantzos@gmail.com>
2025-03-21 11:26:02 +0200

    Create main.cs
    No comment

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+Using System. Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsharpTutorials
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+           string message = "Hello World!!";
+
+           Console.WriteLine)message);
+        }
+     }
+} 
